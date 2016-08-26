c40d308562fe368d1acbea7db07ceba5339d36a4
Alessandro De Angelis <alessandrodea22@gmail.com>
2016-08-26 15:28:11 +0200

    added docs to provider and tuttleuser models, added tests for tuttleuser and provider models

--- django-tuttle/provider/models.py
+++ django-tuttle/provider/models.py
@@ -3,13 +3,19 @@ from django.db import models
 
 
 class Provider(models.Model):
-    name = models.CharField(null=True, max_length=50)
+    """
+    Provider model: stores provider's name
+    """
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Repository(models.Model):
+    """
+    Repository model: stores the name of the repository and the user who own the repo
+    """
     name = models.CharField(max_length=100)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
 
@@ -18,6 +24,9 @@ class Repository(models.Model):
 
 
 class DeployKey(models.Model):
+    """
+    DeployKey model stores the deploy key parameters of a repository
+    """
     title = models.CharField(max_length=255)
     key = models.CharField(max_length=255)
     repository = models.ForeignKey(Repository)
