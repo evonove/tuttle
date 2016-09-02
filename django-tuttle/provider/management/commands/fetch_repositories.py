46d78731306f2f1bf55145f053de4c6e3b38c75b
Girbons <alessandrodea22@gmail.com>
2016-09-02 11:12:55 +0200

    modified django command and added test for it

--- django-tuttle/provider/management/commands/fetch_repositories.py
+++ django-tuttle/provider/management/commands/fetch_repositories.py
@@ -1,42 +1,111 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 from github import Github, BadCredentialsException
-from github import UnknownObjectException
-
 from tuttleuser.models import TuttleUser
 from provider.models import Repository, Provider, DeployKey
+
+logger = logging.getLogger()
 
 
 class Command(BaseCommand):
     help = 'command for get repositories list from github account'
 
+    def add_arguments(self, parser):
+
+        parser.add_argument('-t', '--token', dest='token', help='User token for provider', required=True)
+        parser.add_argument('-p', '--provider', dest='provider', help='Provider\'s name', required=True)
+
     def handle(self, *args, **options):
-        tuttle = TuttleUser.objects.get(username='girbons')
-        provider = Provider.objects.get(name='github')
+
+        token_argument = options['token']
+        provider_argument = options['provider']
+
+        try:
+            tuttle = TuttleUser.objects.get(token=token_argument)
+            provider = Provider.objects.get(name=provider_argument)
+
+        except TuttleUser.DoesNotExist:
+
+            logger.error('User objects doesn\'t exist')
+            raise CommandError('User object doesn\'t exist')
+
+        except Provider.DoesNotExist:
+
+            logger.error('Provider objects doesn\'t exist')
+            raise CommandError('Provider object doesn\'t exist')
 
         try:
             login = Github(tuttle.token)
             user = login.get_user()
 
-            self.stdout.write('saving repository info')
+            self.stdout.write('Saving repository info')
             for repo in user.get_repos():
                 if repo.organization:
-                    Repository.objects.get_or_create(name=repo.name, owner=repo.owner.login,
-                                                     organization=repo.organization.name, is_private=repo.private,
-                                                     user=tuttle, provider=provider)
-                    self.stdout.write(repo.name)
-                else:
-                    Repository.objects.get_or_create(name=repo.name, owner=repo.owner.login, is_private=repo.private,
-                                                     user=tuttle, provider=provider)
-                    self.stdout.write(repo.name)
 
-                if repo.permissions.admin:
+                    params = {
+                        'name': repo.name,
+                        'owner': repo.owner.login,
+                        'organization': repo.organization.name,
+                        'is_private': repo.private,
+                        'user': tuttle,
+                        'provider': provider,
+                    }
+
                     try:
-                        for key in user.get_repo(repo.name).get_keys():
-                            if key:
-                                DeployKey.objects.get_or_create(title=key.title, key=key.key,
-                                                                repository=Repository.objects.get(name=repo.name))
-                    except UnknownObjectException:
-                        self.stdout.write('deploy key not found')
+                        Repository.objects.get_or_create(**params)
+                        self.stdout.write(repo.name)
+
+                    except Repository.MultipleObjectsReturned:
+
+                        msg = 'More than 1 Repository with this params: %s ' % params
+                        logger.error(msg)
+                        raise CommandError(msg, 'Fix the database')
+
+                else:
+
+                    params = {
+                        'name': repo.name,
+                        'owner': repo.owner.login,
+                        'is_private': repo.private,
+                        'user': tuttle,
+                        'provider': provider,
+                    }
+
+                    try:
+
+                        Repository.objects.get_or_create(**params)
+                        self.stdout.write(repo.name)
+
+                    except Repository.MultipleObjectsReturned:
+
+                        msg = 'More than 1 Repository with this params: %s ' % params
+                        logger.error(msg)
+                        raise CommandError(msg, 'Fix the database')
+
+                """
+                You need to be admin of your repository for get the deploy key
+                """
+                if repo.permissions.admin:
+                    for key in repo.get_keys():
+
+                        params = {
+                            'title': key.title,
+                            'key': key.key,
+                            'repository': Repository.objects.get(name=repo.name),
+                        }
+
+                        if key:
+                            try:
+                                DeployKey.objects.get_or_create(**params)
+
+                            except DeployKey.MultipleObjectsReturned:
+
+                                msg = 'More than 1 Deploykey with this params: %s ' % params
+                                logger.error(msg)
+                                raise CommandError(msg, 'Fix the database')
 
         except BadCredentialsException:
-            raise CommandError("can't login on github account, invalid token")
+
+            logger.error('Login error on %s' % provider)
+            raise CommandError('Can\'t login on github account, invalid token')
---
+++ django-tuttle/provider/tests/test_commands/test_fetch_repositories.py
@@ -0,0 +1,314 @@
+import pytest
+import mock
+
+from django.contrib.auth import get_user_model
+from django.core.management import CommandError
+from django.core.management import call_command
+from github import BadCredentialsException
+from github.Repository import Repository as GithubRepo
+from provider.models import Provider, Repository, DeployKey
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_organization_field():
+    """
+    Test the correct creation of Repository object with organization field
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization.name = 'test-organization'
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock get_repos() method
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        Provider.objects.create(name=provider_arg)
+        call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        assert Repository.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_empty_organization_field():
+    """
+    Test the correct creation of Repository object without organization field
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization = None
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock get_repos() method
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        Provider.objects.create(name=provider_arg)
+
+        call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        repository = Repository.objects.get(name='test')
+
+        assert Repository.objects.count() == 1
+        assert repository.organization == ''
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_get_deploykey():
+    """
+    Test the correct creation of Deploy key object
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization = None
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock methods
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+        github_repo_mock.get_keys = mock.MagicMock(return_value=[github_repo_mock])
+
+        # assignment parameters for create deploy key object
+        github_repo_mock.permissions.admin = True
+        github_repo_mock.title = 'test key'
+        github_repo_mock.key = '123456'
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        Provider.objects.create(name=provider_arg)
+        call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        key = DeployKey.objects.get(title='test key')
+        assert DeployKey.objects.count() == 1
+        assert key.title == 'test key'
+        assert key.key == '123456'
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_no_provider_object():
+    # creation of arguments needed for execute the django command
+    token_arg = '123456'
+
+    # creation of object TuttleUser
+    get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+
+    with pytest.raises(CommandError) as ex:
+        call_command('fetch_repositories', '-t', token_arg)
+    assert 'Error: the following arguments are required: -p/--provider' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_no_user_object():
+    # creation of arguments needed for execute the django command
+    provider_arg = 'test'
+
+    # creation of object Provider
+    Provider.objects.create(name=provider_arg)
+
+    with pytest.raises(CommandError) as ex:
+        call_command('fetch_repositories', '-p', provider_arg)
+    assert 'Error: the following arguments are required: -t/--token' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_no_args():
+    with pytest.raises(CommandError) as ex:
+        call_command('fetch_repositories')
+    assert 'Error: the following arguments are required: -t/--token, -p/--provider' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_provider_does_not_exist():
+    # creation of arguments needed for execute the django command
+    token_arg = '123456'
+
+    # creation of object TuttleUser
+    get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+
+    with pytest.raises(CommandError) as ex:
+        call_command('fetch_repositories', '-t', token_arg, '-p', 'provider test')
+    assert 'Provider object doesn\'t exist' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_user_does_not_exist():
+    # creation of arguments needed for execute the django command
+    provider_arg = 'test'
+
+    # creation of object Provider
+    Provider.objects.create(name=provider_arg)
+
+    with pytest.raises(CommandError) as ex:
+        call_command('fetch_repositories', '-t', '11111', '-p', provider_arg)
+    assert 'User object doesn\'t exist' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_with_invalid_token():
+    """
+    Test error failed login
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        githubMock.side_effect = BadCredentialsException(status='', data='')
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        Provider.objects.create(name=provider_arg)
+
+        with pytest.raises(CommandError) as ex:
+            call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        assert 'Can\'t login on github account, invalid token' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_organization_multiple_objects_returned():
+    """
+    Test Error while creating 2 objects with the same params with get_or_create method
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization.name = 'test organization'
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock get_repos() method
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        user = get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        provider = Provider.objects.create(name=provider_arg)
+
+        Repository.objects.create(name='test', owner='user test', organization='test organization',
+                                  is_private=False, user=user, provider=provider)
+
+        Repository.objects.create(name='test', owner='user test', organization='test organization',
+                                  is_private=False, user=user, provider=provider)
+
+        with pytest.raises(CommandError) as ex:
+            call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        assert 'Fix the database' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_without_organization_multiple_objects_returned():
+    """
+    Test Error while creating 2 objects with the same params
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization = None
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock get_repos() method
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        user = get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        provider = Provider.objects.create(name=provider_arg)
+
+        Repository.objects.create(name='test', owner='user test', is_private=False, user=user, provider=provider)
+
+        Repository.objects.create(name='test', owner='user test', is_private=False, user=user, provider=provider)
+
+        with pytest.raises(CommandError) as ex:
+            call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        assert 'Fix the database' in str(ex.value)
+
+
+@pytest.mark.django_db
+def test_fetch_repositories_get_deploykey_multiple_objects_returned():
+    """
+    Test the correct creation of Deploy key object
+    """
+    with mock.patch('provider.management.commands.fetch_repositories.Github') as githubMock:
+        github_user_mock = mock.MagicMock()
+        github_repo_mock = mock.MagicMock(GithubRepo)
+
+        # assignment parameters for the creation of Repository object
+        github_repo_mock.name = 'test'
+        github_repo_mock.organization = None
+        github_repo_mock.owner.login = 'user test'
+        github_repo_mock.private = False
+
+        # mock methods
+        githubMock = githubMock.return_value
+        githubMock.get_user = mock.MagicMock(return_value=github_user_mock)
+        github_user_mock.get_repos = mock.MagicMock(return_value=[github_repo_mock])
+        github_repo_mock.get_keys = mock.MagicMock(return_value=[github_repo_mock])
+
+        # assignment parameters for create deploy key object
+        github_repo_mock.permissions.admin = True
+        github_repo_mock.title = 'test key'
+        github_repo_mock.key = '123456'
+
+        # creation of arguments needed for execute the django command
+        token_arg = '123456'
+        provider_arg = 'test'
+
+        # creation of objects TuttleUser and Provider
+        user = get_user_model().objects.create(username='username', email='test@test.it', token=token_arg)
+        provider = Provider.objects.create(name=provider_arg)
+
+        repository = Repository.objects.create(name='test', owner='user test', is_private=False,
+                                               user=user, provider=provider)
+        DeployKey.objects.create(title='test key', key='123456', repository=repository)
+        DeployKey.objects.create(title='test key', key='123456', repository=repository)
+
+        with pytest.raises(CommandError) as ex:
+            call_command('fetch_repositories', '-t', token_arg, '-p', provider_arg)
+        assert 'Fix the database' in str(ex.value)
