c63162d60a1bb70cde6f6821b81638cdeafb69ab
Girbons <alessandrodea22@gmail.com>
2016-09-07 10:59:26 +0200

    removed redondant class from admin.py of tuttleuser

--- django-tuttle/tuttleuser/admin.py
+++ django-tuttle/tuttleuser/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
-
-
-class TuttleUserForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
-    form = TuttleUserForm
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('token', )}),
     )
